672d8146341ec7120f660e49d63594023e518797
Abdulrahman-Alaa7 <wl3elkobs@gmail.com>
2023-05-20 15:04:30 +0200

    third-commit

--- js/main-trad.js
+++ js/main-trad.js
@@ -291,7 +291,7 @@ function myFunction() {
 
 // CountDown
 // Set the date we're counting down to
-var countDownDate = new Date("may 20, 2023 9:00:00").getTime();
+var countDownDate = new Date("jun 7, 2023 9:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
