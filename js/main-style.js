0f3f5684a94660113da6c3ff2847c60ccdec0429
Abdulrahman-Alaa7 <wl3elkobs@gmail.com>
2023-05-23 21:35:05 +0200

    update

--- js/main-style.js
+++ js/main-style.js
@@ -1,3 +1,25 @@
+let tabs = document.querySelectorAll(".tabs li ");
+let tabsArray = Array.from(tabs);
+let divs = document.querySelectorAll(".content > div");
+let divsArray = Array.from(divs);
+
+// console.log(divsArray);
+
+tabsArray.forEach((ele) => {
+  ele.addEventListener("click", function (e) {
+    tabsArray.forEach((ele) => {
+      ele.classList.remove("active");
+    });
+    e.currentTarget.classList.add("active");
+    divsArray.forEach((div) => {
+      div.style.display = "none";
+    });
+    // console.log(e.currentTarget.dataset.cont);
+    document.querySelector(e.currentTarget.dataset.cont).style.display =
+      "block";
+  });
+});
+
 // One
 
 const spanOne = document.querySelector(".span-one");
